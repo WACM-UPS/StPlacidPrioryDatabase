7b7d9dbbc140aa37277896c7734741fbac2f3d8a
rvhirsch <rhirsch@pugetsound.edu>
2016-04-23 16:54:21 -0700

    fixed links

--- datesinput.php
+++ datesinput.php
@@ -1,4 +1,4 @@
 <?php
-  $db = new PDO('sqlite:/var/www/html/ddl.sql');
+  $db = new PDO('sqlite:/var/www/html/database.db');
   $db->exec('INSERT INTO RegistrationInfo' VALUES(programID, personID, numPeople, startDay, startTime, endDay, endTime));
 ?>
