da96d8578cb4684fd3191fd2d1da45506f9c251c
rvhirsch <rhirsch@pugetsound.edu>
2016-04-23 17:34:00 -0700

    this doesnt quite work but close enough dangit

--- contactinput.php
+++ contactinput.php
@@ -1,9 +1,15 @@
 <?php
-  // prep DB
-  $db = new PDO('sqlite:/var/www/html/database.db');
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $db->exec("INSERT INTO ContactInfo VALUES('$_POST[personID]', '$_POST[fname]', '$_POST[lname]', '$_POST[cellNum]', '$_POST[homeNum]', '$_POST[email]', '$_POST[street]', '$_POST[city]', '$_POST[state]', '$_POST[zip]')");
-  $db=null;
+  error_reporting(E_ERROR | E_PARSE);
+  try {
+    // prep DB
+    $db = new PDO('sqlite:/var/www/html/database.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->exec("INSERT INTO ContactInfo VALUES('$_POST[personID]', '$_POST[fname]', '$_POST[lname]', '$_POST[cellNum]', '$_POST[homeNum]', '$_POST[email]', '$_POST[street]', '$_POST[apartment]', '$_POST[city]', '$_POST[state]', '$_POST[zip]')");
+    $db=null;
 
-  header("Location: /index.html")
+    header("Location: /index.html")
+  }
+  catch(PDOException $e) {
+    echo 'Exception: ' .$e->getMessage();
+  }
 ?>
