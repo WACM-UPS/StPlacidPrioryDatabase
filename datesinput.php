6d391b8fab9655dcb281fae44457445d0dc92ed8
Liana Bardallis <yastaghr@gmail.com>
2016-04-23 17:30:23 -0700

    if mult peps then group

--- datesinput.php
+++ datesinput.php
@@ -3,6 +3,11 @@ error_reporting{E_ERROR | E_PARSE}
 try{
   $db = new PDO('sqlite:/var/www/html/ddl.sql');
   $db->exec("INSERT INTO RegistrationInfo' VALUES('$_POST[programID]', '$_POST[personID]', '$_POST[numPeople]', '$_POST[startDay]', '$_POST[startTime]', '$_POST[endDay]', '$_POST[endTime]')");
+  if ($_POST[personID]>1) {
+    //redirect to group page
+    echo 'You are a group!';
+  }
+
 } catch (PDOException $e){
   echo 'Exception:' $e->getMessage();
 }
