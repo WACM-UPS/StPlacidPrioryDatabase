a03c401fe5f8ff1d77e046733e29367f37125a7e
Liana Bardallis <yastaghr@gmail.com>
2016-04-23 17:17:35 -0700

    dates php

--- datesinput.php
+++ datesinput.php
@@ -1,4 +1,9 @@
 <?php
-  $db = new PDO('sqlite:/var/www/html/database.db');
-  $db->exec("INSERT INTO RegistrationInfo VALUES('$_POST[programID]', '$_POST[personID]', '$_POST[numPeople]', '$_POST[startDay]', '$_POST[startTime]', '$_POST[endDay]', '$_POST[endTime]')");
+error_reporting{E_ERROR | E_PARSE}
+try{
+  $db = new PDO('sqlite:/var/www/html/ddl.sql');
+  $db->exec("INSERT INTO RegistrationInfo' VALUES('$_POST[programID]', '$_POST[personID]', '$_POST[numPeople]', '$_POST[startDay]', '$_POST[startTime]', '$_POST[endDay]', '$_POST[endTime]')");
+} catch (PDOException $e){
+  echo 'Exception:' $e->getMessage();
+}
 ?>
