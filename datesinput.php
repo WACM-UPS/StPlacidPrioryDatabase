d798ae59a46bc3a021d0f74bd9d0b20442fd095c
Liana Bardallis <yastaghr@gmail.com>
2016-04-30 16:51:48 -0700

    still no work but try

--- datesinput.php
+++ datesinput.php
@@ -1,8 +1,10 @@
 <?php
-error_reporting{E_ERROR | E_PARSE}
+error_reporting(E_ERROR | E_PARSE);
 try{
   $db = new PDO('sqlite:/var/www/html/ddl.sql');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->exec("INSERT INTO RegistrationInfo' VALUES('$_POST[programID]', '$_POST[personID]', '$_POST[numPeople]', '$_POST[startDay]', '$_POST[startTime]', '$_POST[endDay]', '$_POST[endTime]')");
+
   if ($_POST[personID]>1) {
     //redirect to group page
     echo 'You are a group!';
@@ -12,3 +14,4 @@ try{
   echo 'Exception:' $e->getMessage();
 }
 ?>
+
